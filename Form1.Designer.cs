17d29aa82108da80cac13dc1b8dbccda9e74bd28
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-01-11 08:44:06 -0700

    Initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ShredPro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbFiles = new System.Windows.Forms.ListBox();
+            this.btnShred = new System.Windows.Forms.Button();
+            this.cmbAlgorithms = new System.Windows.Forms.ComboBox();
+            this.lblTotalSize = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbFiles
+            // 
+            this.lbFiles.FormattingEnabled = true;
+            this.lbFiles.Location = new System.Drawing.Point(145, 103);
+            this.lbFiles.Name = "lbFiles";
+            this.lbFiles.Size = new System.Drawing.Size(339, 108);
+            this.lbFiles.TabIndex = 0;
+            this.lbFiles.DragDrop += new System.Windows.Forms.DragEventHandler(this.lbFiles_DragDrop);
+            this.lbFiles.DragEnter += new System.Windows.Forms.DragEventHandler(this.lbFiles_DragEnter);
+            // 
+            // btnShred
+            // 
+            this.btnShred.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnShred.ForeColor = System.Drawing.Color.Black;
+            this.btnShred.Location = new System.Drawing.Point(490, 103);
+            this.btnShred.Name = "btnShred";
+            this.btnShred.Size = new System.Drawing.Size(75, 23);
+            this.btnShred.TabIndex = 1;
+            this.btnShred.Text = "Shred";
+            this.btnShred.UseVisualStyleBackColor = false;
+            this.btnShred.Click += new System.EventHandler(this.btnShred_Click);
+            // 
+            // cmbAlgorithms
+            // 
+            this.cmbAlgorithms.FormattingEnabled = true;
+            this.cmbAlgorithms.Location = new System.Drawing.Point(490, 132);
+            this.cmbAlgorithms.Name = "cmbAlgorithms";
+            this.cmbAlgorithms.Size = new System.Drawing.Size(182, 21);
+            this.cmbAlgorithms.TabIndex = 2;
+            // 
+            // lblTotalSize
+            // 
+            this.lblTotalSize.AutoSize = true;
+            this.lblTotalSize.Location = new System.Drawing.Point(145, 218);
+            this.lblTotalSize.Name = "lblTotalSize";
+            this.lblTotalSize.Size = new System.Drawing.Size(0, 13);
+            this.lblTotalSize.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTotalSize);
+            this.Controls.Add(this.cmbAlgorithms);
+            this.Controls.Add(this.btnShred);
+            this.Controls.Add(this.lbFiles);
+            this.Name = "Form1";
+            this.Text = "ShredPro | V. 1.0.0 | cvs0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbFiles;
+        private System.Windows.Forms.Button btnShred;
+        private System.Windows.Forms.ComboBox cmbAlgorithms;
+        private System.Windows.Forms.Label lblTotalSize;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShredPro
+{
+    public partial class Form1 : Form
+    {
+        private bool shreddingInProgress = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lbFiles.AllowDrop = true;
+            lbFiles.DragDrop += lbFiles_DragDrop;
+            lbFiles.DragEnter += lbFiles_DragEnter;
+            btnShred.Click += btnShred_Click;
+
+            cmbAlgorithms.Items.Add("Random Data");
+            cmbAlgorithms.Items.Add("Zeroes");
+            cmbAlgorithms.Items.Add("DoD Standard (3 passes)");
+            cmbAlgorithms.Items.Add("Gutmann Method (35 passes)");
+            cmbAlgorithms.SelectedIndex = 0;
+        }
+
+        private void UpdateTotalFileSize()
+        {
+            long totalSize = 0;
+
+            foreach (var item in lbFiles.Items)
+            {
+                string path = item.ToString();
+
+                if (File.Exists(path))
+                {
+                    totalSize += new FileInfo(path).Length;
+                }
+                else if (Directory.Exists(path))
+                {
+                    totalSize += GetDirectorySize(path);
+                }
+            }
+
+            lblTotalSize.Text = $"Total File Size: {FormatFileSize(totalSize)}";
+        }
+
+        private long GetDirectorySize(string path)
+        {
+            long size = 0;
+
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                size += new FileInfo(file).Length;
+            }
+
+            string[] subdirectories = Directory.GetDirectories(path);
+            foreach (string subdirectory in subdirectories)
+            {
+                size += GetDirectorySize(subdirectory);
+            }
+
+            return size;
+        }
+
+        private string FormatFileSize(long sizeInBytes)
+        {
+            string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+            int order = 0;
+
+            while (sizeInBytes >= 1024 && order < sizes.Length - 1)
+            {
+                order++;
+                sizeInBytes = sizeInBytes / 1024;
+            }
+
+            return $"{sizeInBytes:0.##} {sizes[order]}";
+        }
+
+        private void btnShred_Click(object sender, EventArgs e)
+        {
+            if (!shreddingInProgress)
+            {
+                shreddingInProgress = true;
+                DialogResult result = MessageBox.Show("Are you sure you want to shred these files?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    ShredFiles();
+                }
+
+                shreddingInProgress = false;
+            }
+        }
+
+        private void lbFiles_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                if (!lbFiles.Items.Contains(file))
+                {
+                    lbFiles.Items.Add(file);
+                }
+            }
+
+            UpdateTotalFileSize();
+        }
+
+        private void ShredFiles()
+        {
+            string selectedAlgorithm = cmbAlgorithms.SelectedItem.ToString();
+
+            foreach (var item in lbFiles.Items)
+            {
+                string path = item.ToString();
+
+                try
+                {
+                    if (Directory.Exists(path))
+                    {
+                        ShredDirectory(path, selectedAlgorithm);
+                    }
+                    else if (File.Exists(path))
+                    {
+                        ShredFile(path, selectedAlgorithm);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error shredding {path}: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            lbFiles.Items.Clear();
+
+            UpdateTotalFileSize();
+        }
+
+        private void ShredFile(string filePath, string algorithm)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Write))
+                {
+                    ShredData(fs, algorithm);
+                }
+
+                File.Delete(filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error shredding file {filePath}: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ShredDirectory(string directoryPath, string algorithm)
+        {
+            // Shred all files in the directory
+            string[] files = Directory.GetFiles(directoryPath);
+            foreach (string file in files)
+            {
+                ShredFile(file, algorithm);
+            }
+
+            // Recursively shred files in subdirectories
+            string[] subdirectories = Directory.GetDirectories(directoryPath);
+            foreach (string subdirectory in subdirectories)
+            {
+                ShredDirectory(subdirectory, algorithm);
+            }
+
+            // Remove the empty directory
+            Directory.Delete(directoryPath);
+        }
+
+        private void ShredData(FileStream fs, string algorithm)
+        {
+            switch (algorithm)
+            {
+                case "Random Data":
+                    byte[] randomBuffer = new byte[fs.Length];
+                    new Random().NextBytes(randomBuffer);
+                    fs.Write(randomBuffer, 0, randomBuffer.Length);
+                    break;
+
+                case "Zeroes":
+                    byte[] zeroBuffer = new byte[fs.Length];
+                    fs.Write(zeroBuffer, 0, zeroBuffer.Length);
+                    break;
+
+                case "DoD Standard (3 passes)":
+                    byte[] dodBuffer = new byte[fs.Length];
+
+                    fs.Write(dodBuffer, 0, dodBuffer.Length);
+
+                    for (int i = 0; i < dodBuffer.Length; i++)
+                    {
+                        dodBuffer[i] = 0xFF;
+                    }
+                    fs.Write(dodBuffer, 0, dodBuffer.Length);
+
+                    new Random().NextBytes(dodBuffer);
+                    fs.Write(dodBuffer, 0, dodBuffer.Length);
+                    break;
+
+                case "Gutmann Method (35 passes)":
+                    byte[] gutmannBuffer = new byte[fs.Length];
+
+                    for (int pass = 0; pass < 35; pass++)
+                    {
+                        for (int i = 0; i < gutmannBuffer.Length; i++)
+                        {
+                            gutmannBuffer[i] = (byte)pass;
+                        }
+                        fs.Write(gutmannBuffer, 0, gutmannBuffer.Length);
+                    }
+                    break;
+
+                default:
+                    throw new ArgumentException("Invalid shredding algorithm");
+            }
+        }
+
+        private void lbFiles_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+    }
+}
